1b0e63f7ca48a5e4a21b29fd99d49c1de5bcb598
CloneZero1 <bryanreisjucade.deus@gmail.com>
2026-02-12 01:08:39 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+const CACHE_NAME = "gym-elite-v3";
 
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
